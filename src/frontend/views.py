8701a87c76b8aa8d49702f972f36cc399182b2fe
William Montgomery <10781353+wimo7083@users.noreply.github.com>
2018-11-04 08:15:27 -0700

    remove commented code.

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -50,8 +50,7 @@ def get_logo_and_users(logo: InMemoryUploadedFile, url_root: str) -> Tuple[str, 
         users = '@wimo7083 @AllenAnthes,'
         logo_url = f'https://pyback.ngrok.io/static/media/logos/{school_logo}'
     else:
-        users = '@wimo7083 ,@jhampton ,@kylemh'
-        # users = ''
+        users = '@wimo7083 ,@jhampton ,@kylemh'        
         logo_url = f'{url_root}static/logos/{school_logo}'
     return logo_url, users
 
